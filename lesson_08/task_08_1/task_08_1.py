d9848a1e6b14576470035b6957fdc3f6e5351dc6
Joe <totenlicht@mail.ru>
2022-06-08 11:57:09 +0500

    Solved task_08_2 in lesson 08
    Update task_08_1 in lesson 08

--- lesson_08/task_08_1/task_08_1.py
+++ lesson_08/task_08_1/task_08_1.py
@@ -1,18 +1,4 @@
-import re
-
-
-def email_parse(email_address):
-    data_email_domain = re.compile(r'''(?P<username>^[a-zA-Z\d'._+-]+?)
-                                       @
-                                       (?P<domain>[a-zA-Z_.-]*\.[a-zA-Z]{2})
-                                       ''', re.VERBOSE)
-    try:
-        data_email_domain.search(email_address).groupdict()
-    except AttributeError:
-        raise ValueError
-    else:
-        return data_email_domain.search(email_address).groupdict()
-
+from functions import email_parse
 
 with open('task_8_1_test_email.txt', mode='r', encoding='utf-8') as file:
     for line in file:
